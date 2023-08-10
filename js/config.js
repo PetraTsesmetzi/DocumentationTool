8c9d0189f320e33a0fd8a23227879fe2514bf360
Petra Tsesmetzi <p_tsesmetzi@gmx.de>
2023-08-10 17:18:44 +0200

    just a commit

--- js/config.js
+++ js/config.js
@@ -1,3 +1,3 @@
-export const API_URL='http://localhost:56319/DocumentationsTool/php/controler.php';
+export const API_URL='http://localhost:49247/DocumentationsTool/php/controler.php';
 
 // export const API_URL='https://documentationtool.000webhostapp.com/php/controler.php';
