868641a38aefaa38ced64517c8823229390d6150
Petra Tsesmetzi <p_tsesmetzi@gmx.de>
2023-08-20 16:25:48 +0200

    config verändert zum testen für -gitignore

--- php/config.php
+++ php/config.php
@@ -10,4 +10,7 @@ const DB_PASSWD = '';
 
 
 
-
+//const DB_DSN = 'mysql:host=localhost;dbname=id20993951_docutool';
+//
+//const DB_USER = 'id20993951_docutool';
+//const DB_PASSWD = 'Tsesmetzi_1974';
